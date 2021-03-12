c286a4d762c2efbfea6e18a55218df568ce68be1
iamalmiir <78283120+iamalmiir@users.noreply.github.com>
2021-03-12 14:25:45 -0500

    Update hangman.py

--- day7/hangman.py
+++ day7/hangman.py
@@ -35,7 +35,6 @@ while not end_of_game:
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
-    #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
